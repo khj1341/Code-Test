2186d4f8292126de2488fc73c05f51d1f53c37bc
gimhyeonjae <hyunjae1341@gmail.com>
2021-07-26 15:51:59 +0900

    CodeTest - 두개 합치기

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useRef, useState } from 'react';
 
 export default function App() {
-  const ref = useRef<HTMLDivElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
 
     const [doughnutCnt, setDoughnutCnt] = useState<number>(1);
 
@@ -13,17 +13,16 @@ export default function App() {
         left: '200px',
         position: 'absolute',
         zIndex: 1000,
-        backgroundColor: 'transparent',
-        border: '1px solid black'
+        backgroundColor: 'blue',
     };
 
     return (
         <>
-            <div style={divStyle} onMouseOver={(e: React.MouseEvent) => { 
+            <div style={divStyle} onMouseOver={(e: React.MouseEvent) => {
                 if (ref && ref.current) {
                     ref.current.style.zIndex = '0';
-                    ref.current.style.left = e.nativeEvent.pageX-140 + "px";
-                    ref.current.style.top = e.nativeEvent.pageY-140 + "px";
+                    ref.current.style.left = e.nativeEvent.pageX - 140 + "px";
+                    ref.current.style.top = e.nativeEvent.pageY - 140 + "px";
                 }
 
                 if (ref?.current?.style && ref?.current?.style.left !== '') {
@@ -40,6 +39,30 @@ export default function App() {
                     );
                 })
             }
+
+            <div style={{marginTop: '700px'}}>
+                <div className="division-element-1">
+                    Division Element #1
+                </div>
+                <div className="division-element-2">
+                    Division Element #2
+                </div>
+                <div className="division-element-3">
+                    Division Element #3
+                </div>
+                <div className="division-element-4">
+                    Division Element #4
+                </div>
+                <div className="division-element-5">
+                    Division Element #5
+                </div>
+                <div className="division-element-6">
+                    Division Element #6
+                </div>
+                <div className="division-element-7">
+                    Division Element #7
+                </div>
+            </div>
         </>
     )
 }
